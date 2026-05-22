5a355fc1fb7c3396f58887fd6ef283d6fc7f1c52
ibappam <ibappam@users.noreply.github.com>
2026-05-22 11:05:19 +0000

    deploy: 8bc123c9b97fd3f288706228ed2333025b67cdd9

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-var movieData = { d: "https://loffe414wil.com", t: "1779440039171" };
+var movieData = {
+  "d": "https://loffe414wil.com",
+  "r": "https://www.5movierulz.soccer/",
+  "t": "2026-05-22T11:05:18.830Z"
+};
