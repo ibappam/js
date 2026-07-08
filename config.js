0d98e6c600db92077071e57c4ea8941ed4a4f734
ibappam <ibappam@users.noreply.github.com>
2026-07-08 16:37:52 +0000

    deploy: 07aa1888e1dad397d7239b79a55faa759ccec5fd

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var movieData = {
   "d": "https://gemma416okl.com",
-  "r": "https://www.5movierulz.limited/",
-  "t": "2026-07-08T14:03:10.377Z"
+  "r": "https://www.5movierulz.support/",
+  "t": "2026-07-08T16:37:51.848Z"
 };
