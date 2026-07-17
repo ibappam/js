cc2832399c01804981ae49a18816511dbd2286df
ibappam <ibappam@users.noreply.github.com>
2026-07-17 19:05:54 +0000

    deploy: 07aa1888e1dad397d7239b79a55faa759ccec5fd

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var movieData = {
   "d": "https://gemma416okl.com",
-  "r": "https://www.5movierulz.support/",
-  "t": "2026-07-17T16:14:26.483Z"
+  "r": "https://www.5movierulz.vote/",
+  "t": "2026-07-17T19:05:53.604Z"
 };
