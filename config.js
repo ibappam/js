9c3b1a2cf930ca1565b60d2cd02f45f54631c5c5
ibappam <ibappam@users.noreply.github.com>
2026-08-13 04:49:27 +0000

    deploy: 07aa1888e1dad397d7239b79a55faa759ccec5fd

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var movieData = {
   "d": "https://rasta428jem.com",
-  "r": "https://www.5movierulz.vacations/",
-  "t": "2026-08-13T01:51:41.357Z"
+  "r": "https://www.5movierulz.watch/",
+  "t": "2026-08-13T04:49:27.253Z"
 };
