e9ca88333745ee7fc127684e4d4ef109e9a70dec
ibappam <ibappam@users.noreply.github.com>
2026-09-20 20:21:45 +0000

    deploy: 07aa1888e1dad397d7239b79a55faa759ccec5fd

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var movieData = {
   "d": "https://slast430did.com",
-  "r": "https://www.5movierulz.solutions/",
-  "t": "2026-09-20T15:29:26.370Z"
+  "r": "https://www.5movierulz.fitness/",
+  "t": "2026-09-20T20:21:45.001Z"
 };
